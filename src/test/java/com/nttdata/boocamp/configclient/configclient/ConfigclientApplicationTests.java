8661676453350b6cf44beb1ea0c672b9ace7327f
AdrianGomezLuna <adrigoluna@gmail.com>
2021-12-22 15:23:17 +0100

    first commit

---
+++ src/test/java/com/nttdata/boocamp/configclient/configclient/ConfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.boocamp.configclient.configclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
